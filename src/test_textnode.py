ffeddd1eefe999ef17bd6a8995d172a520fecb71
zafir <zafirchowdhury69@gmail.com>
2025-12-01 15:19:08 +0600

    Add TextNode Tests

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        node2 = TextNode("This is a bold text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    
+    def test_not_eq(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        node2 = TextNode("This is a italic text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    
+    def test_url_none(self):
+        node = TextNode("This is not a url", TextType.TEXT)
+        self.assertEqual(node.url, None)
+
+
+    def test_print(self):
+        node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+        self.assertEqual(repr(node), 'TextNode(This is some anchor text, link, https://www.boot.dev)')
+
+if __name__ == "__main__":
+    unittest.main()
