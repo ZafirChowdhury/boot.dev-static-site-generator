853f632099339b47b29e1e310b4c3f366ed0080a
zafir <zafirchowdhury69@gmail.com>
2025-12-04 22:02:54 +0600

    Add markdown regex

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,8 @@
 from textnode import TextType, TextNode
 from leafnode import LeafNode
 
+import re
+
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
         return LeafNode(tag=None, value=text_node.text)
@@ -53,3 +55,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(nodes)
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
